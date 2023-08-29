454779feac49c1471608525c2b68eaff0c96180f
이도하 <victoryddh5@naver.com>
2023-08-29 09:37:14 +0900

    로그인 관련 테스트 코드 작성

--- src/main/java/com/example/security6/domain/dto/user/LoginDto.java
+++ src/main/java/com/example/security6/domain/dto/user/LoginDto.java
@@ -1,8 +1,10 @@
 package com.example.security6.domain.dto.user;
 
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 public class LoginDto {
 
     private String userId;
--- src/test/java/com/example/security6/user/UserServiceTest.java
+++ src/test/java/com/example/security6/user/UserServiceTest.java
@@ -2,13 +2,12 @@ package com.example.security6.user;
 
 
 import com.example.security6.domain.dto.user.JoinDto;
+import com.example.security6.domain.dto.user.LoginDto;
 import com.example.security6.exception.AppException;
-import com.example.security6.repository.UserRepository;
 import com.example.security6.service.UserService;
 import org.junit.Assert;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.rules.ExpectedException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,7 +32,7 @@ public class UserServiceTest {
     }
 
     @Test
-    @DisplayName("회원가입 아이디 중복 에러")
+    @DisplayName("회원가입 실패 - 아이디 중복")
     public void 회원가입_아이디_중복_에러(){
 
         JoinDto joinDto = JoinDto.builder()
@@ -50,9 +49,68 @@ public class UserServiceTest {
 
         userService.save(joinDto);
 
+        System.out.println("첫번째 회원 회원가입 성공");
 
         Assert.assertThrows(AppException.class, () -> {
             userService.save(newJoinDto);
+        });
+
+    }
+
+    @Test
+    @DisplayName("로그인 성공")
+    public void 로그인_성공(){
+
+        JoinDto joinDto = JoinDto.builder()
+                .userId("testId")
+                .userPassword("testPassword")
+                .userName("testName")
+                .build();
+
+        userService.save(joinDto);
+
+        LoginDto loginDto = LoginDto.builder()
+                .userId("testId")
+                .userPassword("testPassword")
+                .build();
+
+        userService.login(loginDto);
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 없는 아이디")
+    public void 로그인_실패_없는_아이디(){
+
+        LoginDto loginDto = LoginDto.builder()
+                .userId("testId")
+                .userPassword("testPassword")
+                .build();
+
+        Assert.assertThrows(AppException.class, () -> {
+            userService.login(loginDto);
+        });
+
+    }
+
+    @Test
+    @DisplayName("로그인 실패 - 비밀번호 오류")
+    public void 로그인_실패_비밀번호_오류(){
+
+        JoinDto joinDto = JoinDto.builder()
+                .userId("testId")
+                .userPassword("testPassword")
+                .userName("testName")
+                .build();
+
+        userService.save(joinDto);
+
+        LoginDto loginDto = LoginDto.builder()
+                .userId("testId")
+                .userPassword("invalidPassword")
+                .build();
+
+        Assert.assertThrows(AppException.class, () -> {
+            userService.login(loginDto);
         });
 
     }
